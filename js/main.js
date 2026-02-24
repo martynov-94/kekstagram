f8f7ce557ab556df93a5b2c1d3ee73698f66c57c
martynov-94 <31200896+martynov-94@users.noreply.github.com>
2026-02-24 11:35:44 +0300

    Добавляет функции для генерации данных

---
+++ js/main.js
@@ -0,0 +1,117 @@
+const COMMENTS_SENTENCES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Антон',
+  'Витя',
+  'Илья',
+  'Алена',
+  'Аня',
+  'Максим',
+  'Джозеф',
+  'Валерия'
+];
+
+const photosIDs = {
+  min: 1,
+  max: 25,
+  usedIDs: new Set()
+};
+
+const commentsIDs = {
+  min: 1,
+  max: 10000,
+  usedIDs: new Set()
+};
+
+function getRandomInteger(start, end) {
+  return Math.floor(Math.random() * (end - start + 1)) + start;
+}
+
+function generateID(data) {
+  while (true) {
+    const randomID = getRandomInteger(data.min, data.max);
+
+    if (data.usedIDs.has(randomID)) {
+      continue;
+    }
+
+    data.usedIDs.add(randomID);
+    return randomID;
+  }
+}
+
+function generatePhotoDescription() {
+  return 'Красивое описание фотографии';
+}
+
+function getPhotoLikes() {
+  return getRandomInteger(15, 300);
+}
+
+function getPhotoComments() {
+  const numberOfComments = getRandomInteger(0, 30);
+  return Array.from({ length: numberOfComments }, createComment);
+}
+
+function getCommentAvatar() {
+  return `img/avatar-${getRandomInteger(1, 6)}.svg`;
+}
+
+function getCommentMessage() {
+  const commentSentences = [];
+  const numberOfSentences = Math.min(getRandomInteger(1, 2), COMMENTS_SENTENCES.length);
+  const usedSentences = new Set();
+
+  while (commentSentences.length < numberOfSentences) {
+    const sentence = getRandomSentence();
+    if (usedSentences.has(sentence)) {
+      continue;
+    }
+
+    usedSentences.add(sentence);
+    commentSentences.push(sentence);
+  }
+
+  return commentSentences.join` `;
+}
+
+function getRandomSentence() {
+  return COMMENTS_SENTENCES[getRandomInteger(0, COMMENTS_SENTENCES.length)];
+}
+
+function getCommentName() {
+  return NAMES[getRandomInteger(0, NAMES.length)];
+}
+
+function createComment() {
+  return {
+    id: generateID(commentsIDs),
+    avatar: getCommentAvatar(),
+    message: getCommentMessage(),
+    name: getCommentName()
+  };
+}
+
+function createPhoto() {
+  const photo = {
+    id: generateID(photosIDs),
+    description: generatePhotoDescription(),
+    likes: getPhotoLikes(),
+    comments: getPhotoComments()
+  };
+
+  photo.url = `photos/${photo.id}.jpg`;
+
+  return photo;
+}
+
+
+const photos = Array.from({ length: 25 }, createPhoto);
+console.log(photos);
